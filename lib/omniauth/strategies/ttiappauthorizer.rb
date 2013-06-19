4cf36c8f9502742d9788d996bf20135294e63eb7
Brian Dear <superacidjax@me.com>
2013-06-19 14:54:35 -0500

    sets green site URL

--- lib/omniauth/strategies/ttiappauthorizer.rb
+++ lib/omniauth/strategies/ttiappauthorizer.rb
@@ -7,7 +7,7 @@ module OmniAuth
       option :name, 'ttiappauthorizer'
 
       option :client_options, {
-        site: "http://vanilla-red.herokuapp.com/",
+        site: "http://vanilla-green.herokuapp.com/",
         authorize_url: "/oauth/authorize"
       }
 
