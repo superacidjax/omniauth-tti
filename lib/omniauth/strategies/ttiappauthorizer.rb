f27850cf9cdf6864b6da88f06dc319e0b44bc1ae
Brian Dear <superacidjax@me.com>
2013-06-18 22:36:16 -0500

    adds red creds

--- lib/omniauth/strategies/ttiappauthorizer.rb
+++ lib/omniauth/strategies/ttiappauthorizer.rb
@@ -7,7 +7,7 @@ module OmniAuth
       option :name, 'ttiappauthorizer'
 
       option :client_options, {
-        site: "http://localhost:3000",
+        site: "http://vanilla-red.herokuapp.com/",
         authorize_url: "/oauth/authorize"
       }
 
